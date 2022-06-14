5295b8b5eb6ab4a51d35f73930f24ffd46ac1af2
SophrosyneEunoia <kelvinmulyawan2000@gmail.com>
2022-06-14 18:45:24 +0700

    add : link

--- main/js/login.js
+++ main/js/login.js
@@ -3,7 +3,7 @@ function login_checker() {
 	var password = document.getElementById('password').value;
 
 	if (username === 'kelvin' && password === 'kelvin123') {
-		window.location = "https://docs.google.com/document/d/1MzeKSoAGoB8BNOFjABQbhM2sgr8uQ3s3/edit?usp=sharing&ouid=108974529682508068135&rtpof=true&sd=true"
+		window.location = "file.html"
 	} else {
 		window.location = "../index.html"
 	}
